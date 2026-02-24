1f394c967be1a09860dc5c6f3a7b42dc7656ae18
juancastamy <45299603+juancastamy@users.noreply.github.com>
2026-02-24 22:44:42 +0100

    start calibration file

---
+++ src/calibration.cpp
@@ -0,0 +1,116 @@
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+#include <opencv2/objdetect/aruco_detector.hpp>
+#include <iostream>
+#include "opencv2/imgcodecs.hpp"
+#include <string>
+#include <map>
+#include <filesystem>
+
+// Dictionary that maps the string input to the actual dictionary
+static int dictFromString(const std::string& name){
+    static const std::map<std::string,int> m = {
+        {"DICT_4X4_50", cv::aruco::DICT_4X4_50},
+        {"DICT_4X4_100", cv::aruco::DICT_4X4_100},
+        {"DICT_4X4_250", cv::aruco::DICT_4X4_250},
+        {"DICT_4X4_1000", cv::aruco::DICT_4X4_1000},
+
+        {"DICT_5X5_50", cv::aruco::DICT_5X5_50},
+        {"DICT_5X5_100", cv::aruco::DICT_5X5_100},
+        {"DICT_5X5_250", cv::aruco::DICT_5X5_250},
+        {"DICT_5X5_1000", cv::aruco::DICT_5X5_1000},
+
+        {"DICT_6X6_50", cv::aruco::DICT_6X6_50},
+        {"DICT_6X6_100", cv::aruco::DICT_6X6_100},
+        {"DICT_6X6_250", cv::aruco::DICT_6X6_250},
+        {"DICT_6X6_1000", cv::aruco::DICT_6X6_1000},
+
+        {"DICT_7X7_50", cv::aruco::DICT_7X7_50},
+        {"DICT_7X7_100", cv::aruco::DICT_7X7_100},
+        {"DICT_7X7_250", cv::aruco::DICT_7X7_250},
+        {"DICT_7X7_1000", cv::aruco::DICT_7X7_1000},
+
+        {"DICT_ARUCO_ORIGINAL", cv::aruco::DICT_ARUCO_ORIGINAL}
+    };
+
+    auto it = m.find(name);
+    if (it == m.end())
+        throw std::runtime_error("Unknown dictionary: " + name);
+
+    return it->second;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc < 4)
+    {
+        std::cout << "Usage: ./calibration <video_source_no> <dictionary_name> <if_capture>" << std::endl;
+        return -1;
+    }
+
+    // std::cout << std::atoi(argv[1]) << std::endl;
+    cv::VideoCapture webCam(std::atoi(argv[1])); // VideoCapture object declaration. Usually 0 is the integrated, 2 is the first external USB one↪→
+
+    if (webCam.isOpened() == false)
+    { // Check if the VideoCapture object has been correctly associated to the webcam↪→
+        std::cerr << "error: Webcam could not be connected." << std::endl;
+        return -1;
+    }
+
+    cv::Mat imgOriginal;  // input image
+
+    // Process parameters.
+    std::string dictionaryName = argv[2];
+    static int dictionary_name = dictFromString(dictionaryName);
+
+    char charCheckForESCKey{0};
+    char charCheckForCKey{0};
+    int nframe = 0;
+
+    cv::namedWindow("imgOriginal");
+
+    while (charCheckForESCKey != 27 && webCam.isOpened())
+    {                                                 // loop until ESC key is pressed or webcam is lost
+        bool frameSuccess = webCam.read(imgOriginal); // get next frame from input stream
+
+        if (!frameSuccess || imgOriginal.empty())
+        { // if the frame was not read or read wrongly
+            std::cerr << "error: Frame could not be read." << std::endl;
+            break;
+        }
+        charCheckForCKey = cv::waitKey(1);
+        if (charCheckForCKey == 'c' && std::atoi(argv[3]) == 1)
+        {
+            printf("Capturing image %d\n", nframe);
+            std::string fileName = "/home/juan/Documents/IFROS/perception/LAB1Git/Lab1_Perception/images/image_" + std::to_string(nframe) + ".jpg";
+            printf("Saving image to %s\n", fileName.c_str());
+            cv::imwrite(fileName, imgOriginal);
+            nframe++;
+        }
+        else if(charCheckForCKey == 's' && std::atoi(argv[3]) == 1)
+        {
+            std::string image_set_path = "home/juan/Documents/IFROS/perception/LAB1Git/Lab1_Perception/images/";
+        }
+        /*
+        // create matrix to store the marker corners and ids
+        std::vector<int> markerIds;
+        std::vector<std::vector<cv::Point2f>> markerCorners, rejectedCandidates;
+
+        // create the parametes for the detector and get the dictionary to use.
+        cv::aruco::DetectorParameters detectorParams = cv::aruco::DetectorParameters();
+        cv::aruco::Dictionary dictionary = cv::aruco::getPredefinedDictionary(dictionary_name);
+        cv::aruco::ArucoDetector detector(dictionary, detectorParams);
+
+        // detect the markers on the imageand draw the markers detected
+        detector.detectMarkers(imgOriginal, markerCorners, markerIds, rejectedCandidates);
+        cv::Mat outputImage = imgOriginal.clone();
+        cv::aruco::drawDetectedMarkers(outputImage, markerCorners, markerIds);
+
+        // Show output video results windows*/
+        cv::imshow("imgOriginal", imgOriginal);
+
+        charCheckForESCKey = cv::waitKey(1); // gets the key pressed
+    }
+    return 0;
+}
